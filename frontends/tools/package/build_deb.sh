0ee01b5ba498891dc6ffc3d0bf0e11d406ca72e4
Andreas T Jonsson <mail@andreasjonsson.se>
2018-02-05 12:07:05 +0100

    Fixed some build issues.

--- frontends/tools/package/build_deb.sh
+++ frontends/tools/package/build_deb.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#DIR=fantasim-sdl_$FANTASIM_SDL_VERSION-$TRAVIS_BUILD_NUMBER
-DIR=fantasim-sdl_$FANTASIM_SDL_VERSION
+DIR=fantasim-sdl_linux
 
 if [ -d "$DIR" ]; then rm -rf "$DIR"; fi
-cp -r "frontends/tools/package/fantasim-$FANTASIM_SDL_VERSION-x" $DIR
+cp -r "frontends/tools/package/fantasim-sdl.deb" $DIR
 
 mkdir -p $DIR/usr/local/bin
-cp fantasim-sdl $DIR/usr/local/bin
+cp frontends/sdl/fantasim-sdl $DIR/usr/local/bin
 
 rpl e34f19fc-289d-4fb9-b134-ced07a29a273 $FANTASIM_SDL_VERSION $DIR/DEBIAN/control
 rpl e34f19fc-299d-4fb9-b334-aed07b29a273 $TRAVIS_BUILD_NUMBER $DIR/DEBIAN/control
