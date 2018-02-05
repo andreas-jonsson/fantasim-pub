4f263809b20a4881a74d50e69d4760fe55a28847
Andreas T Jonsson <mail@andreasjonsson.se>
2018-02-05 10:58:02 +0100

    Addd scripts for OSX build.

--- frontends/tools/package/build_app.sh
+++ frontends/tools/package/build_app.sh
@@ -6,5 +6,5 @@ cp -r "frontends/tools/package/Fantasim-SDL.app" Fantasim-SDL.app
 cp fantasim-sdl Fantasim-SDL.app/Contents/MacOS
 dylibbundler -od -b -x Fantasim-SDL.app/Contents/MacOS/fantasim-sdl -d Fantasim-SDL.app/Contents/libs
 
-zip -rq fantasim-sdl-osx.zip Fantasim-SDL.app
+zip -rq fantasim-sdl_osx.zip Fantasim-SDL.app
 rm -rf Fantasim-SDL.app
