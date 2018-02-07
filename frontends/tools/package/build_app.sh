b0c504e87868cc2dbb9aa144827313f7f3a137e0
Andreas T Jonsson <mail@andreasjonsson.se>
2018-02-07 14:46:30 +0100

    Update build_app.sh

--- frontends/tools/package/build_app.sh
+++ frontends/tools/package/build_app.sh
@@ -3,7 +3,7 @@
 if [ -d Fantasim-SDL.app ]; then rm -rf Fantasim-SDL.app; fi
 cp -r "frontends/tools/package/Fantasim-SDL.app" Fantasim-SDL.app
 
-cp fantasim-sdl Fantasim-SDL.app/Contents/MacOS
+cp frontends/sdl/fantasim-sdl Fantasim-SDL.app/Contents/MacOS
 dylibbundler -od -b -x Fantasim-SDL.app/Contents/MacOS/fantasim-sdl -d Fantasim-SDL.app/Contents/libs
 
 zip -rq fantasim-sdl_osx.zip Fantasim-SDL.app
