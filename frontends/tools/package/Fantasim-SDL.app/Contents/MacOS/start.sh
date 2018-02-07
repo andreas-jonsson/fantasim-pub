f7bc8bf9882581de49abc0311f290d33ae06b1bb
Andreas T Jonsson <mail@andreasjonsson.se>
2018-02-07 15:10:17 +0100

    Update start.sh

--- frontends/tools/package/Fantasim-SDL.app/Contents/MacOS/start.sh
+++ frontends/tools/package/Fantasim-SDL.app/Contents/MacOS/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-osascript -e 'tell app "Terminal" to do script "\"'"$(dirname "$0")/fantasim-sdl"'\""'
+osascript -e 'tell app "Terminal" to do script "\"'"$(dirname "$0")/fantasim-sdl -url="$1""'\""'
