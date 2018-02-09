b27a770ccd4f784ca3e2d9f0ed81f66e883289c9
Andreas Jonsson <mail@andreasjonsson.se>
2018-02-09 11:07:40 +0100

    New OSX package.

--- frontends/tools/package/Fantasim-SDL.app/Contents/MacOS/start.sh
+++ frontends/tools/package/Fantasim-SDL.app/Contents/MacOS/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-$(dirname "$0")/fantasim-sdl -url="$1"
+# echo $0 > /Users/andreasjonsson/Desktop/test.txt
+# echo $1 >> /Users/andreasjonsson/Desktop/test.txt
+
+# $(dirname "$0")/fantasim-sdl -url="$1"
+
+osascript <<EOD
+	on open location this_URL
+		do shell script "/Users/andreasjonsson/Downloads/Fantasim-SDL/Contents/MacOS/tramp.sh '" & this_URL & "'"		
+	end open location
+EOD
