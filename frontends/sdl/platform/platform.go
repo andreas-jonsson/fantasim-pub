59a9ac44a5841b4620eaffabd28cf26cb2fe9b0f
Andreas Jonsson <mail@andreasjonsson.se>
2018-03-27 08:23:04 +0200

    Updated snap build.

--- frontends/sdl/platform/platform.go
+++ frontends/sdl/platform/platform.go
@@ -67,16 +67,7 @@ func InitSDL(windowSize, resolution image.Point, fullscreen bool) (*SDL, error) 
 		return nil, err
 	}
 
-	fmt.Println("Video drivers:")
-	if num, err := sdl.GetNumRenderDrivers(); err == nil {
-		for i := 0; i < num; i++ {
-			var info sdl.RendererInfo
-			sdl.GetRenderDriverInfo(i, &info)
-			fmt.Println(info.Name)
-		}
-	}
-
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	renderer, err := sdl.CreateRenderer(window, -1, 0)
 	if err != nil {
 		sdl.Quit()
 		window.Destroy()
