6f2404a8ca8f3858a1342fd7b90fa2727eea634b
Andreas T Jonsson <mail@andreasjonsson.se>
2019-03-31 21:54:41 +0200

    Removed old vendor folder.

--- frontends/vendor/github.com/veandco/go-sdl2/sdl/sdl_cgo.go
+++
@@ -1,7 +0,0 @@
-// +build !static
-
-package sdl
-
-//#cgo windows LDFLAGS: -lSDL2
-//#cgo linux freebsd darwin pkg-config: sdl2
-import "C"
--- frontends/vendor/github.com/veandco/go-sdl2/sdl/sdl_wrapper.h
+++
@@ -1,6 +0,0 @@
-#if defined(_WIN32)
-	#include <SDL2/SDL.h>
-	#include <stdlib.h>
-#else
-	#include <SDL.h>
-#endif
